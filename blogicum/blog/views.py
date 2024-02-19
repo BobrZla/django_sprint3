e69f8923b96a83dadf31cbc55952acf397eacfa3
Dmitry <bobrzla@yandex.ru>
2024-02-19 14:21:13 +0300

    pre_relise

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,68 +1,46 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.utils import timezone as tz
 
-posts = [
-    {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
-                страшным штормом, потерпел крушение.
-                Весь экипаж, кроме меня, утонул; я же,
-                несчастный Робинзон Крузо, был выброшен
-                полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
-    },
-    {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                с мели приливом и пригнало гораздо ближе к берегу.
-                Это подало мне надежду, что, когда ветер стихнет,
-                мне удастся добраться до корабля и запастись едой и
-                другими необходимыми вещами. Я немного приободрился,
-                хотя печаль о погибших товарищах не покидала меня.
-                Мне всё думалось, что, останься мы на корабле, мы
-                непременно спаслись бы. Теперь из его обломков мы могли бы
-                построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
-    },
-    {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
-                порывистый ветер. 25 октября.  Корабль за ночь разбило
-                в щепки; на том месте, где он стоял, торчат какие-то
-                жалкие обломки,  да и те видны только во время отлива.
-                Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
-    },
-]
+from blog.models import Post, Category
 
 
-post_ids = [post['id'] for post in posts]
+def postfilter():
+    return Post.objects.select_related(
+        'category',
+        'location',
+        'author',
+    ).filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=tz.now()
+    )
 
 
 def index(request):
     template = 'blog/index.html'
-    context = {'posts_list': reversed(posts)}
+    post_list = postfilter()[:5]
+    context = {'post_list': post_list}
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
-    if post_id not in post_ids:
-        raise Http404('Прости, такого поста нет, видимо его еще не написали.')
     template = 'blog/detail.html'
-    context = {'post': posts[post_id]}
+    post = get_object_or_404(
+        postfilter(),
+        pk=post_id
+    )
+    context = {'post': post}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context = {'category_slug': category_slug}
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    posts = postfilter().filter(category__slug=category_slug)
+    context = {'category': category, 'post_list': posts}
     return render(request, template, context)
